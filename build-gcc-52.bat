863bd5162c87bb3ba0a5438345342e3d5c3248a8
steve donovan <steve.j.donovan@gmail.com>
2012-01-09 11:45:43 +0200

    update batch files

--- build-gcc-52.bat
+++ build-gcc-52.bat
@@ -1,5 +1,5 @@
 REM compiling for mingw against msvcrt
-set LUA_DIR=C:\Users\steve\lua\lua-5.2.0\src
+set LUA_DIR=D:\dev\lua\lua-5.2.0\src
 set CFLAGS=-c -O1 -DPSAPI_VERSION=1  -I"%LUA_DIR%"
 gcc %CFLAGS% winapi.c
 gcc %CFLAGS% wutils.c
